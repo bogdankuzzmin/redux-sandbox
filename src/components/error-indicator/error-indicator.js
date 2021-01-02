19a5d5f2ce93b33022330123a34f5f5fd2216f99
Bogdan <bogdankuzzmin@gmail.com>
2021-01-03 00:46:40 +0600

    created error-indicator component

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ErrorIndicator = () => {
+  return (
+    <div>
+      <span>
+        Something went wrong.
+      </span>
+    </div>
+  );
+};
+
+export default ErrorIndicator;
