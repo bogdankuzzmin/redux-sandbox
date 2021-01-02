10573d3c41aa33217db8fbfae79c7508310e1be2
Bogdan <bogdankuzzmin@gmail.com>
2021-01-03 00:38:33 +0600

    created loader component

---
+++ src/components/loader/loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import loader from './load.svg';
+
+const Loader = () => {
+  return (
+    <img src={loader} alt="load load animation"/>
+  );
+}
+
+export default Loader;
