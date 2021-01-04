966641875316c9cf1ea3b8f62258701a1a7c9f0d
Bogdan <bogdankuzzmin@gmail.com>
2021-01-05 00:12:20 +0600

    deleted data from reducer - initialState

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,5 @@
 const initialState = {
-  books: [
-    {
-      id: 1,
-      title: 'Harry Potter and the Chamber of Secrets',
-      author: 'J.K. Rowling',
-    },
-    {
-      id: 2,
-      title: '7 Навыков Высокоэффективных Людей',
-      author: 'Стивен Р. Кови',
-    }
-  ]
+  books: []
 };
 
 const reducer = (state = initialState, action) => {
