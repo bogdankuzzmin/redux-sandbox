a2dd341456b4e52b37833624e211ac1e2d6fef6d
Bogdan <bogdankuzzmin@gmail.com>
2021-01-07 23:47:19 +0600

    added two values to bookstore-service data, made an imitation of receiving data from the server

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,16 +1,27 @@
 export default class BookstoreService {
+
+  data = [
+    {
+      id: 1,
+      title: 'Harry Potter and the Chamber of Secrets',
+      author: 'J.K. Rowling',
+      price: 32,
+      coverImage: 'https://images-na.ssl-images-amazon.com/images/I/81YOuOGFCJL.jpg',
+    },
+    {
+      id: 2,
+      title: '7 Навыков Высокоэффективных Людей',
+      author: 'Стивен Р. Кови',
+      price: 44,
+      coverImage: 'https://i4.mybook.io/p/512x852/book_covers/52/a2/52a2b562-ac47-4663-96b8-485e634a8d5d.jpg?v2',
+    }
+  ];
+
   getBooks() {
-    return [
-      {
-        id: 1,
-        title: 'Harry Potter and the Chamber of Secrets',
-        author: 'J.K. Rowling',
-      },
-      {
-        id: 2,
-        title: '7 Навыков Высокоэффективных Людей',
-        author: 'Стивен Р. Кови',
-      }
-    ];
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.data)
+      }, 700);
+    });
   }
 }
